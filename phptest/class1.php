147c40ac76cb24a4c062b33852cf6a32859ea078
Mark van der Laan <markkvdlaan93@gmail.com>
2015-05-25 21:07:50 +0200

    OOP php oefenen

--- phptest/class1.php
+++ phptest/class1.php
@@ -1,2 +1,31 @@
 <?php 
+
+class Test1
+{
+	private $prop1 = 10;
+
+	function setProp1($newVar)
+	{
+		$this->$prop1 = $newVar; 
+	}
+
+	function getProp1()
+	{
+		return $this->$prop1;
+	}
+
+	function sum()
+	{
+		$a = 5; $b = 10;
+		return $a + $b;
+	}
+}
+
+$obj1 = new Test1();
+echo $obj1->getProp1;
+$obj1->setProp1 = 10;
+echo $obj1->getProp1;
+
+// var_dump($obj1);
+
 ?>
